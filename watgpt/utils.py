c6f68ad4deb35ae1d4b0479fc371d8e4dadf3ba0
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-02-16 17:02:52 +0100

    Add logger to utils

--- watgpt/utils.py
+++ watgpt/utils.py
@@ -12,3 +12,23 @@ handler.setFormatter(
 )
 handler.setLevel(logging.INFO)
 logger.addHandler(handler)
+
+
+def log_debug(*args, **kwargs):
+    """Log an debug message."""
+    logger.debug(*args, **kwargs)
+
+
+def log_info(*args, **kwargs):
+    """Log an info message."""
+    logger.info(*args, **kwargs)
+
+
+def log_warning(*args, **kwargs):
+    """Log a warning message."""
+    logger.warning(*args, **kwargs)
+
+
+def log_error(*args, **kwargs):
+    """Log an error message."""
+    logger.error(*args, **kwargs)
