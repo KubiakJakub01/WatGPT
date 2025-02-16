858db0c47943fb1b6a2941c4611253b85e31c8b3
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-02-16 15:56:53 +0100

    Add more constants

--- watgpt/db/constants.py
+++ watgpt/db/constants.py
@@ -2,3 +2,8 @@ from pathlib import Path
 
 DATA_DIR_PATH = Path('wat_data')
 TIMETABLE_URL = 'https://planzajec.wcy.wat.edu.pl/pl/rozklad?grupa_id={group}'
+CALENDAR_PDF_FP = (
+    DATA_DIR_PATH
+    / 'zal._nr_1_organizacja_zajec_w_roku_akademickim_2024_2025_na_studiach_stacjonarnych.pdf'
+)
+STRUCTURED_PDF_FP = DATA_DIR_PATH / 'informator_dla_studentow_1_roku_2024.pdf'
