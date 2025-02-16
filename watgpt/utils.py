e087fdc9b34cc46f2a576fe7ffbb7e40c31c8a9d
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-02-16 17:11:24 +0100

    Add debug file logger

--- watgpt/utils.py
+++ watgpt/utils.py
@@ -3,15 +3,15 @@ import logging
 import coloredlogs
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('debug.log', mode='w', encoding='utf-8')
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+file_handler.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
-handler.setFormatter(
-    coloredlogs.ColoredFormatter(
-        fmt='%(asctime)s :: %(levelname)s :: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-    )
-)
+handler.setFormatter(coloredlogs.ColoredFormatter('%(asctime)s %(levelname)s %(message)s'))
 handler.setLevel(logging.INFO)
 logger.addHandler(handler)
+logger.addHandler(file_handler)
 
 
 def log_debug(*args, **kwargs):
